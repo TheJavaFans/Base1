a55a10879ac1f91284bb3795d72085e877e3b4b3
TheJavaFans <42144342+TheJavaFans@users.noreply.github.com>
2018-08-12 14:52:12 +0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -11,7 +11,10 @@ public class MainClass{
     public static void main(String[] args){
         Obj obj1 = new Obj(10);
         Obj obj2 = new Obj(10.5f);
-        Obj obj3 = new Obj(75.5d);
+        Obj obj3 = new Obj(75.5);
         Obj obj4 = new Obj('a');
-    }
+        System.out.println(obj1);
+        System.out.println(obj2);
+        System.out.println(obj3);
+        System.out.println(obj4);
 }
